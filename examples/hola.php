416f7a194af1539d7610a71df7410a0aea11af86
Federico <lograssofede@gmail.com>
2018-04-15 20:04:04 -0300

    Update hola.php

--- examples/hola.php
+++ examples/hola.php
@@ -6,6 +6,15 @@
 
 <?php
 echo "Hello World!";
+ require '../Meli/meli.php';
+require '../configApp.php';
+$meli = new Meli($appId, $secretKey);
+
+$url = '/users/226384143/';
+$result = $meli->get($url);
+echo '<pre>';
+print_r($result);
+echo '</pre>';
 ?>
 
 </body>
