7f77561569535300651fa8505aafd401908664d4
Federico <lograssofede@gmail.com>
2018-04-15 19:52:04 -0300

    Update hola.php

--- examples/hola.php
+++ examples/hola.php
@@ -1,14 +1,4 @@
 <p> Hola </p>
 <?php
-//require '../Meli/meli.php';
-//require '../configApp.php';
-
-$meli = new Meli($appId, $secretKey);
-$params = array();
-$url = ' questions/search';
-echo $url
-$result = $meli->get($url, array('item'=>'MLA-617699510','access_token' => $_SESSION['access_token']));
-echo '<pre>';
-print_r($result);
-echo '</pre>';
+echo "Hola";
 ?>
