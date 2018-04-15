c77c91615df3bc144f00ae77623b893216671f42
Federico <lograssofede@gmail.com>
2018-04-15 20:01:20 -0300

    Update hola.php

--- examples/hola.php
+++ examples/hola.php
@@ -5,16 +5,8 @@
 <h1>My first PHP page</h1>
 
 <?php
-require '../Meli/meli.php';
-require '../configApp.php';
-$meli = new Meli($appId, $secretKey);
-$params = array();
-$url = '/users/226384143/'
-$result = $meli->get($url);
-echo '<pre>';
-print_r($result);
-echo '</pre>';
-
+echo "Hello World!";
+?>
 
 </body>
 </html>
