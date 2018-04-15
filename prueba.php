fa5a4306f63e5ae90e6f40feb72a981dce1290dc
Federico <lograssofede@gmail.com>
2018-04-15 19:46:14 -0300

    Update prueba.php

--- prueba.php
+++ prueba.php
@@ -5,6 +5,7 @@ require '../configApp.php';
 $meli = new Meli($appId, $secretKey);
 $params = array();
 $url = ' questions/search';
+echo $url
 $result = $meli->get($url, array('item'=>'MLA-617699510','access_token' => $_SESSION['access_token']));
 echo '<pre>';
 print_r($result);
