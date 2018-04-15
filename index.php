54110994d888b290d0d1760e41fb5460cb4097f5
Federico <lograssofede@gmail.com>
2018-04-15 19:24:58 -0300

    Update index.php

--- index.php
+++ index.php
@@ -239,25 +239,7 @@ $appName = explode('.', $domain)[0];
 
             <hr>
 
-            <div class="row">
-                <h3>Your Credentials</h3>
-                <div class="row-info col-sm-3 col-md-3">
-                    <b>App_Id: </b>
-                    <?php echo $appId; ?>
-                </div>
-                <div class="row-info col-sm-3 col-md-3">
-                    <b>Secret_Key: </b>
-                    <?php echo $secretKey; ?>
-                </div>
-                <div class="row-info col-sm-3 col-md-3">
-                    <b>Redirect_URI: </b>
-                    <?php echo $redirectURI; ?>
-                </div>
-                <div class="row-info col-sm-3 col-md-3">
-                    <b>Site_Id: </b>
-                    <?php echo $siteId; ?>
-                </div>
-            </div>
+
             <hr>
         </main>
     </body>
