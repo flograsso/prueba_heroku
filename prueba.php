f570b281ec33f2e9d6955da04a293d62d022cd50
Federico Lo Grasso <lograssofede@gmail.com>
2018-04-16 12:24:47 -0300

    a

--- prueba.php
+++ prueba.php
@@ -8,14 +8,19 @@
 
 
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-echo $url;
+
 $server = $url["host"];
 $username = $url["bb531b11e3bba4"];
 $password = $url["66f4ca4f"];
 $db = substr($url["path"], 1);
 echo $db;
+echo $server;
 $conn = new mysqli($server, $username, $password, $db);
-
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
 
 
 ?>
