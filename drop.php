83132650d30424204e0cf0c21d50f34745a87722
Federico Lo Grasso <lograssofede@gmail.com>
2018-04-16 18:50:22 -0300

    flograsso

---
+++ drop.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+    <html lang="en" >
+
+
+<?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+echo $db;
+echo $server;
+$conn = new mysqli($server, $username, $password, $db);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+
+$sql = "DROP TABLE `tabla1` ";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+?>
