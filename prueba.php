7c4311abd7b56cd8b6629ef1d38b546a6d3c0432
Federico Lo Grasso <lograssofede@gmail.com>
2018-04-16 12:17:50 -0300

    A

--- prueba.php
+++ prueba.php
@@ -12,5 +12,17 @@ $url = ' questions/search';
 echo $url;
 $result = $meli->get($url, array('item'=>'MLA-617699510','access_token' => $_SESSION['access_token']));
 echo '<pre>';
-print_r($result);
+
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["bb531b11e3bba4"];
+$password = $url["66f4ca4f"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+
+
+
 ?>
