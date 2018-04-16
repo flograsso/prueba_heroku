6f18b22d144d44811252adee7903d13dc6fe3887
Federico Lo Grasso <lograssofede@gmail.com>
2018-04-16 12:20:43 -0300

    a

--- prueba.php
+++ prueba.php
@@ -15,12 +15,12 @@ echo '<pre>';
 
 
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
+echo $url;
 $server = $url["host"];
 $username = $url["bb531b11e3bba4"];
 $password = $url["66f4ca4f"];
 $db = substr($url["path"], 1);
-
+echo $db;
 $conn = new mysqli($server, $username, $password, $db);
 
 
