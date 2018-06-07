c18cf4a712b26c83acfd845d9579799a4780898b
Federico Lo Grasso <lograssofede@gmail.com>
2018-06-07 14:01:57 -0300

    a

--- connection.php
+++ connection.php
@@ -2,16 +2,16 @@
 
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 //database
-define('DB_HOST', $url["host"]);
-define('DB_USERNAME', $url["user"]);
-define('DB_PASSWORD', $url["pass"]);
-define('DB_NAME', substr($url["path"], 1));
+$DB_HOST=$url["host"];
+$DB_USERNAME=$url["user"];
+$DB_PASSWORD=$url["pass"];
+$DB_NAME=substr($url["path"], 1);
 
 //get connection
 //(MySQLi Object-Oriented)
 
-
-$conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+global $conn;
+$conn = new mysqli($DB_HOST, $DB_USERNAME, $DB_PASSWORD, $DB_NAME);
 
 if(!$conn){
 	die("Connection failed: " . $conn->error);
