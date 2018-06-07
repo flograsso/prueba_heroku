a6a34ed203ca2c4b7b36538a567f4e7d0a4347ba
Federico Lo Grasso <lograssofede@gmail.com>
2018-06-07 14:03:51 -0300

    a

--- dbFunctions.php
+++ dbFunctions.php
@@ -1,6 +1,23 @@
 
 <?php
-include "connection.php";
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+//database
+$DB_HOST=$url["host"];
+$DB_USERNAME=$url["user"];
+$DB_PASSWORD=$url["pass"];
+$DB_NAME=substr($url["path"], 1);
+
+//get connection
+//(MySQLi Object-Oriented)
+
+global $conn;
+$conn = new mysqli($DB_HOST, $DB_USERNAME, $DB_PASSWORD, $DB_NAME);
+
+if(!$conn){
+	die("Connection failed: " . $conn->error);
+}
+
 
 function setValueDb($table, $field, $value)
 {
