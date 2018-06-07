3cb29b9bc8975763b27c4cab1f1b8e5dbe49c08e
Federico Lo Grasso <lograssofede@gmail.com>
2018-06-07 14:07:36 -0300

    a

--- dbFunctions.php
+++ dbFunctions.php
@@ -21,7 +21,7 @@ if(!$conn){
 
 function setValueDb($table, $field, $value)
 {
-
+    global $conn;
     $sql="INSERT INTO `$table` (`$field`) VALUES ('$value');";
     echo "INSERT INTO `$table` (`$field`) VALUES ('$value');";
     $conn->query($sql);
@@ -30,13 +30,14 @@ function setValueDb($table, $field, $value)
 
 function updateAllValuesDb($table, $field, $value)
 {
+    global $conn;
     $sql="UPDATE `$table` SET '$field'='$value' WHERE 1;";
     $conn->query($sql); 
 }
 
 function getValueDb($table, $field)
 {
-
+    global $conn;
     $sql="SELECT `$field` FROM `$table` WHERE 1;";
     $result = $conn->query($sql);
     $outp = array();
