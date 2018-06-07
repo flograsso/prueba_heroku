aa0f404f5f564cfafd463779db1da5e811237e89
Federico Lo Grasso <lograssofede@gmail.com>
2018-06-07 14:05:01 -0300

    a

--- dbFunctions.php
+++ dbFunctions.php
@@ -11,7 +11,7 @@ $DB_NAME=substr($url["path"], 1);
 //get connection
 //(MySQLi Object-Oriented)
 
-global $conn;
+
 $conn = new mysqli($DB_HOST, $DB_USERNAME, $DB_PASSWORD, $DB_NAME);
 
 if(!$conn){
