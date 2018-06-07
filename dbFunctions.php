066ace695ea791e349d090ff39b6d94d97e898f6
Federico Lo Grasso <lograssofede@gmail.com>
2018-06-07 14:32:26 -0300

    a

--- dbFunctions.php
+++ dbFunctions.php
@@ -9,6 +9,7 @@ function setValueDb($table, $field, $value)
     global $conn;
     $sql="INSERT INTO `$table` (`$field`) VALUES ('$value');";
     $conn->query($sql);
+    echo "valor agregado";
 
 }
 
