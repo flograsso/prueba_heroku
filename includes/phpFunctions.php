b0eeabd00e22a24e0d1248e8c2b1fa2592a15f7a
Federico Lo Grasso <lograssofede@gmail.com>
2018-06-08 14:01:43 -0300

    a

--- includes/phpFunctions.php
+++ includes/phpFunctions.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once ('includes/Meli/meli.php');
-require_once ('includes/configApp.php');
-include ("includes/dbFunctions.php");
+require_once ('/includes/Meli/meli.php');
+require_once ('/includes/configApp.php');
+include ("/includes/dbFunctions.php");
 
 function sec_session_start() {
     $session_name = 'meliSession';   // Configura un nombre de sesión personalizado.
@@ -79,6 +79,85 @@ function login($email, $password, $mysqli) {
             // El usuario no existe.
             return false;
         }
+    }
+}
+
+function login_check($mysqli) {
+    // Revisa si todas las variables de sesión están configuradas.
+    if (isset($_SESSION['user_id'], 
+                        $_SESSION['username'], 
+                        $_SESSION['login_string'])) {
+ 
+        $user_id = $_SESSION['user_id'];
+        $login_string = $_SESSION['login_string'];
+        $username = $_SESSION['username'];
+ 
+        // Obtiene la cadena de agente de usuario del usuario.
+        $user_browser = $_SERVER['HTTP_USER_AGENT'];
+ 
+        if ($stmt = $mysqli->prepare("SELECT password 
+                                      FROM users 
+                                      WHERE id = ? LIMIT 1")) {
+            // Une “$user_id” al parámetro.
+            $stmt->bind_param('i', $user_id);
+            $stmt->execute();   // Ejecuta la consulta preparada.
+            $stmt->store_result();
+ 
+            if ($stmt->num_rows == 1) {
+                // Si el usuario existe, obtiene las variables del resultado.
+                $stmt->bind_result($password);
+                $stmt->fetch();
+                $login_check = hash('sha512', $password . $user_browser);
+ 
+                if ($login_check == $login_string) {
+                    // ¡¡Conectado!! 
+                    return true;
+                } else {
+                    // No conectado.
+                    return false;
+                }
+            } else {
+                // No conectado.
+                return false;
+            }
+        } else {
+            // No conectado.
+            return false;
+        }
+    } else {
+        // No conectado.
+        return false;
+    }
+}
+
+function esc_url($url) {
+ 
+    if ('' == $url) {
+        return $url;
+    }
+ 
+    $url = preg_replace('|[^a-z0-9-~+_.?#=!&;,/:%@$\|*\'()\\x80-\\xff]|i', '', $url);
+ 
+    $strip = array('%0d', '%0a', '%0D', '%0A');
+    $url = (string) $url;
+ 
+    $count = 1;
+    while ($count) {
+        $url = str_replace($strip, '', $url, $count);
+    }
+ 
+    $url = str_replace(';//', '://', $url);
+ 
+    $url = htmlentities($url);
+ 
+    $url = str_replace('&amp;', '&#038;', $url);
+    $url = str_replace("'", '&#039;', $url);
+ 
+    if ($url[0] !== '/') {
+        // Solo nos interesan los enlaces relativos de  $_SERVER['PHP_SELF']
+        return '';
+    } else {
+        return $url;
     }
 }
 
