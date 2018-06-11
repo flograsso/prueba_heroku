dfd39226d70ed734757e7f0ae8a7b1f72a8810f4
Federico Lo Grasso <lograssofede@gmail.com>
2018-06-11 14:13:30 -0300

    a

--- protected_page.php
+++ protected_page.php
@@ -19,6 +19,7 @@ sec_session_start();
                 include ("includes/example_login.php");
                 $json=getAllValuesDb('questions');
                 $params = array();
+                $valores = array();
                 $array = json_decode( $json, true );
                 foreach($array as $item) { //foreach element in $arr
                     $url = '/questions/' . $item['idPregunta'];
@@ -31,7 +32,8 @@ sec_session_start();
                         print_r($result);
                         echo '</pre>';
                         
-                       echo "VAR=" .  $result["body"]["text"];
+                        $valores = json_decode($result["body"], true );
+                       echo "VAR=" .  $valores["text"];
 
                     }
                     else
