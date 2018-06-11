9d5e12100a49f8f0dc99a162bbe0e0c0402aaa45
Federico Lo Grasso <lograssofede@gmail.com>
2018-06-11 09:40:20 -0300

    a

--- eventReceiver.php
+++ eventReceiver.php
@@ -3,30 +3,44 @@
 <?php
 
 include ("includes/example_login.php");
+global $conn;
 
 $data = json_decode(file_get_contents('php://input'), true);
 http_response_code(200);
 
-    $id = ($data["user_id"]);
-    $topic=($data["topic"]);
-    $resource=($data["resource"]);
-    $attempts=($data["attempts"]);
+
+$topic=($data["topic"]);
+$resource=($data["resource"]);
+
+switch ($topic) {
+
+    case "questions":
+        $resource= ereg_replace("[^0-9]", "", $resource);
+        setValueDb("questions","idPregunta,textoPregunta,estadoPregunta,fechaRecibida,textoRespuesta,fechaRespuesta,idUsuario,idItem,demoraRtaSeg","'$resource',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL");
+        break;
+
+    default:
+        $str = "{";
+        foreach ($data as $key => $value) {
+            $str=$str ."$key" . ":" . "$value" . ",";
+        }
+        $str=$str."}";
+        setValueDb("preguntas","idPregunta,textoPregunta,estadoPregunta,fechaRecibida,textoRespuesta,fechaRespuesta,idUsuario,idItem,demoraRtaSeg","'1','$topic','$resource',NULL,'$str',NULL,NULL,NULL,NULL");
+
+
+}
 
 $date1='2018-04-05';
 $date2='2018-04-05';
 
-$str = "{";
-foreach ($data as $key => $value) {
-    $str=$str ."$key" . ":" . "$value" . ",";
-}
-$str=$str."}";
-
-
-
-global $conn;
-
-setValueDb("preguntas","idPregunta,textoPregunta,estadoPregunta,fechaRecibida,textoRespuesta,fechaRespuesta,idUsuario,idItem,demoraRtaSeg","'1','$topic','$resource',NULL,'$str',NULL,NULL,NULL,NULL");
 
 $conn->close;
+
+
+
+
+
+
+
 
 ?>
