6aa6f5940f1e0995034d729f2ece95596195b43e
Federico Lo Grasso <lograssofede@gmail.com>
2018-06-11 15:11:31 -0300

    a

--- includes/phpFunctions.php
+++ includes/phpFunctions.php
@@ -175,7 +175,7 @@ function procesarPregunta($idPregunta)
     {
         $answer=$result["body"]->answer;
         $from=$result["body"]->from;
-        setValueDb("questions","idPregunta,textoPregunta,estadoPregunta,fechaRecibida,textoRespuesta,fechaRespuesta,idUsuario,idItem,demoraRtaSeg,cantPreguntasUsuario","'$resource',".$result["body"] ->text .",".$result["body"] ->status.",". $result["body"] ->date_created . ",". $answer->text . "," .$answer->date_created . ",". $from->id . "," . $result["body"] ->item_id . "," ."" . ",". $from->answered_questions ");
+        setValueDb("questions","idPregunta,textoPregunta,estadoPregunta,fechaRecibida,textoRespuesta,fechaRespuesta,idUsuario,idItem,demoraRtaSeg,cantPreguntasUsuario","'$resource',".$result["body"] ->text .",".$result["body"] ->status.",". $result["body"] ->date_created . ",". $answer->text . "," .$answer->date_created . ",". $from->id . "," . $result["body"] ->item_id . "," ."" . ",". $from->answered_questions);
 
 
     }
