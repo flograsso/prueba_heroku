38d43be1bd9a40f66a4237cb31cbbd150c75dc4a
Federico Lo Grasso <lograssofede@gmail.com>
2018-06-12 10:51:06 -0300

    a

--- protected_page.php
+++ protected_page.php
@@ -58,38 +58,20 @@ sec_session_start();
         <?php if (login_check($mysqli) == true) { 
                 include ("includes/example_login.php");
                 ?>
-
-        <form class="form-horizontal" action="/action_page.php">
-            <div class="form-group">
-                <label class="control-label col-sm-2" for="email">Email:</label>
+            <form class="form-horizontal" >
+            <div>
+                <label>Consulta</label>
                 <div class="col-sm-10">
-                    <input type="email" class="form-control" id="email" placeholder="Enter email">
+                    <input class="form-control" id="consulta" placeholder="Query">
                 </div>
             </div>
-            <div class="form-group">
-                <label class="control-label col-sm-2" for="pwd">Password:</label>
-                <div class="col-sm-10">
-                    <input
-                        type="password"
-                        class="form-control"
-                        id="pwd"
-                        placeholder="Enter password">
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="col-sm-offset-2 col-sm-10">
-                    <div class="checkbox">
-                        <label><input type="checkbox">
-                            Remember me</label>
-                    </div>
-                </div>
-            </div>
-            <div class="form-group">
+            <div>
                 <div class="col-sm-offset-2 col-sm-10">
                     <button type="submit" class="btn btn-default">Submit</button>
                 </div>
             </div>
         </form>
+        </div>
     <?php
                 }
                
